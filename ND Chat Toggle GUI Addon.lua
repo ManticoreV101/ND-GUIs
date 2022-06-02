db52f33eb29ddfa918318027f6e1fd360a1ca169
Blue's Hue <76952738+ManticoreV101@users.noreply.github.com>
2022-06-02 14:21:00 +0800

    Update ND Chat Toggle GUI Addon.lua

--- ND Chat Toggle GUI Addon.lua
+++ ND Chat Toggle GUI Addon.lua
@@ -1,166 +1,71 @@
+
 local lib = loadstring(game:HttpGet"https://fluxteam.xyz/external-files/lib.lua")()
-local window = lib:CreateWindow("Speech Toggle")
+local window = lib:CreateWindow('Chat Utility')
 
-      local Players = game:GetService("Players")
-      local lplay = Players.LocalPlayer
-      local PlayerScripts = lplay ~= nil and lplay:FindFirstChild('PlayerScripts') or nil
-      local ChatScript = PlayerScripts ~= nil and PlayerScripts:FindFirstChild('ChatScript') or nil
-      local ChatMain = ChatScript ~= nil and ChatScript:FindFirstChild('ChatMain') or nil
-      
-      function Quote(text)
-      local text = tostring("` `"..text.."` `")
-      return text
-      end
-      
-      local function InitialiseReplace(ChatScript)
-      local ChatMain = require(ChatScript:WaitForChild("ChatMain"))
-      local ChatBar = debug.getupvalue(ChatMain.FocusChatBar, 1)
-      local TextBox = ChatBar:GetTextBox()
-      local LastChangedText = tick()
-      local PreviousText = ""
-      
-      TextBox:GetPropertyChangedSignal("Text"):Connect(function()
-          LastChangedText = tick()
-          end)
-      
-      local Checker = coroutine.create(function()
-          local ImprovedText = Quote(TextBox.Text)
-          TextBox.Text = ImprovedText
-          PreviousText = ImprovedText
-          end)
-      coroutine.resume(Checker)
-      return Checker
-      end
-      
-      local tog = false
-      
-      local bool = false
-      function Caller()
-      if bool == true then return end
-      if tog then
-      bool = true
-      InitialiseReplace(ChatScript)
-      wait()
-      bool = false
-      end
-      end
-      
-      local ChatMains = require(ChatScript:WaitForChild("ChatMain"))
-      local ChatBar = debug.getupvalue(ChatMains.FocusChatBar, 1)
-      local TextBox = ChatBar:GetTextBox()
-      TextBox.FocusLost:Connect(Caller)
-      
-      window:AddToggle("Toggle", function(t)
-      tog = t
-      end)
+local Players = game:GetService("Players")
+local lplay = Players.LocalPlayer
+local PlayerScripts = lplay ~= nil and lplay:FindFirstChild('PlayerScripts') or nil
+local ChatScript = PlayerScripts ~= nil and PlayerScripts:FindFirstChild('ChatScript') or nil
+local ChatMain = ChatScript ~= nil and ChatScript:FindFirstChild('ChatMain') or nil
 
-local window = lib:CreateWindow("Action Toggle")
+local Quote = nil
+local Line = nil
+local Square = nil
 
-      local Players = game:GetService("Players")
-      local lplay = Players.LocalPlayer
-      local PlayerScripts = lplay ~= nil and lplay:FindFirstChild('PlayerScripts') or nil
-      local ChatScript = PlayerScripts ~= nil and PlayerScripts:FindFirstChild('ChatScript') or nil
-      local ChatMain = ChatScript ~= nil and ChatScript:FindFirstChild('ChatMain') or nil
-      
-      function Quote(text1)
-      local text1 = tostring("|| "..text1.." ||")
-      return text1
-      end
-      
-      local function InitialiseReplace(ChatScript)
-      local ChatMain = require(ChatScript:WaitForChild("ChatMain"))
-      local ChatBar = debug.getupvalue(ChatMain.FocusChatBar, 1)
-      local TextBox = ChatBar:GetTextBox()
-      local LastChangedText = tick()
-      local PreviousText = ""
-      
-      TextBox:GetPropertyChangedSignal("Text"):Connect(function()
-          LastChangedText = tick()
-          end)
-      
-      local Checker = coroutine.create(function()
-          local ImprovedText = Quote(TextBox.Text)
-          TextBox.Text = ImprovedText
-          PreviousText = ImprovedText
-          end)
-      coroutine.resume(Checker)
-      return Checker
-      end
-      
-      local tog1 = false
-      
-      local bool1 = false
-      function Caller()
-      if bool1 == true then return end
-      if tog1 then
-      bool1 = true
-      InitialiseReplace(ChatScript)
-      wait()
-      bool1 = false
-      end
-      end
-      
-      local ChatMains = require(ChatScript:WaitForChild("ChatMain"))
-      local ChatBar = debug.getupvalue(ChatMains.FocusChatBar, 1)
-      local TextBox = ChatBar:GetTextBox()
-      TextBox.FocusLost:Connect(Caller)
-      
-      window:AddToggle("Toggle", function(r)
-      tog1 = r
-      end)
+function Mode(text)
+if Quote then
+local text = tostring("` `"..text.."` `")
+return text
+end
+if Line then
+local text = tostring('|| '..text..' ||')
+return text
+end
+if Square then
+local text = tostring('['..text..']')
+return text
+end
+end
 
+local function InitialiseReplace(ChatScript)
+local ChatMain = require(ChatScript:WaitForChild("ChatMain"))
+local ChatBar = debug.getupvalue(ChatMain.FocusChatBar, 1)
+local TextBox = ChatBar:GetTextBox()
+local LastChangedText = tick()
+local PreviousText = ""
 
-local window = lib:CreateWindow("OOC Toggle")
+TextBox:GetPropertyChangedSignal("Text"):Connect(function()
+    LastChangedText = tick()
+    end)
 
-      local Players = game:GetService("Players")
-      local lplay = Players.LocalPlayer
-      local PlayerScripts = lplay ~= nil and lplay:FindFirstChild('PlayerScripts') or nil
-      local ChatScript = PlayerScripts ~= nil and PlayerScripts:FindFirstChild('ChatScript') or nil
-      local ChatMain = ChatScript ~= nil and ChatScript:FindFirstChild('ChatMain') or nil
-      
-      function Quote(text2)
-      local text2 = tostring("["..text2.."]")
-      return text2
-      end
-      
-      local function InitialiseReplace(ChatScript)
-      local ChatMain = require(ChatScript:WaitForChild("ChatMain"))
-      local ChatBar = debug.getupvalue(ChatMain.FocusChatBar, 1)
-      local TextBox = ChatBar:GetTextBox()
-      local LastChangedText = tick()
-      local PreviousText = ""
-      
-      TextBox:GetPropertyChangedSignal("Text"):Connect(function()
-          LastChangedText = tick()
-          end)
-      
-      local Checker = coroutine.create(function()
-          local ImprovedText = Quote(TextBox.Text)
-          TextBox.Text = ImprovedText
-          PreviousText = ImprovedText
-          end)
-      coroutine.resume(Checker)
-      return Checker
-      end
-      
-      local tog2 = false
-      
-      local bool2 = false
-      function Caller()
-      if bool2 == true then return end
-      if tog2 then
-      bool2 = true
-      InitialiseReplace(ChatScript)
-      wait()
-      bool2 = false
-      end
-      end
-      
-      local ChatMains = require(ChatScript:WaitForChild("ChatMain"))
-      local ChatBar = debug.getupvalue(ChatMains.FocusChatBar, 1)
-      local TextBox = ChatBar:GetTextBox()
-      TextBox.FocusLost:Connect(Caller)
-      
-      window:AddToggle("Toggle", function(e)
-      tog2 = e
-      end)
+local Checker = coroutine.create(function()
+    local ImprovedText = Mode(TextBox.Text)
+    print(ImprovedText,"Text")
+    TextBox.Text = ImprovedText
+    PreviousText = ImprovedText
+    end)
+coroutine.resume(Checker)
+return Checker
+end
+
+function Caller()
+game:GetService("StarterGui"):SetCore("DevConsoleVisible",true)
+InitialiseReplace(ChatScript)
+end
+
+local ChatMains = require(ChatScript:WaitForChild("ChatMain"))
+local ChatBar = debug.getupvalue(ChatMains.FocusChatBar, 1)
+local TextBox = ChatBar:GetTextBox()
+TextBox.FocusLost:Connect(Caller)
+
+window:AddToggle("Speech", function(t)
+Quote = t
+end)
+
+window:AddToggle("Action", function(t)
+Line = t
+end)
+
+window:AddToggle("OOC", function(t)
+Square = t
+end)
