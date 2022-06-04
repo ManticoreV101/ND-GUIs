e83fde2e5285eb46e90f274936c8d12d6bfb9959
Blue's Hue <76952738+ManticoreV101@users.noreply.github.com>
2022-06-04 20:45:43 +0800

    Add files via upload

---
+++ ND Parkour GUI Addon.lua
@@ -0,0 +1,63 @@
+local lib = loadstring(game:HttpGet"https://fluxteam.xyz/external-files/lib.lua")()
+window:AddButton("Vault Over Counter",function()
+      
+    local args = {
+        [1] = "I now vault over the counter, setting myself on the other side",
+        [2] = "All"
+    }
+    
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end)
+window:AddButton("Vault Over Desk",function()
+      
+        local args = {
+            [1] = "I now vault over the desk, position myself on the other side",
+            [2] = "All"
+        }
+        
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        end)
+window:AddButton("Vault Over Table",function()
+      
+            local args = {
+                [1] = "I now vault over the table, laying myself on the other side",
+                [2] = "All"
+            }
+            
+            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+            end)
+window:AddButton("Vault Over Ledge",function()
+      
+                local args = {
+                    [1] = "I now vault over the ledge, placing myself on the other side",
+                    [2] = "All"
+                }
+                
+                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                end)
+window:AddButton("Jump Off Ledge",function()
+      
+                    local args = {
+                        [1] = "I now jump off the ledge, sending into the air",
+                        [2] = "All"
+                    }
+                    
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                    wait(1.8)
+      
+                    local args = {
+                        [1] = "I compose myself as I fall swiftly towards the ground",
+                        [2] = "All"
+                    }
+                    
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                    end)       
+window:AddButton("Fall Roll",function()
+      
+                    local args = {
+                        [1] = "I now roll as to minimize the shock from the impact",
+                        [2] = "All"
+                    }
+                    
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+                    end)
