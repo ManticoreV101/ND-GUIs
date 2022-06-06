7e514104c83f20b86d4e9ea1f86bc6023c953ec4
Blue's Hue <76952738+ManticoreV101@users.noreply.github.com>
2022-06-06 08:56:51 +0800

    Add files via upload

---
+++ Sci-Fi Combat GUI Addon.lua
@@ -0,0 +1,261 @@
+
+local lib = loadstring(game:HttpGet "https://fluxteam.xyz/external-files/lib.lua")()
+local window = lib:CreateWindow("Sci-Fi Combat")
+window:AddButton(
+    "IC [Activate]",
+    function()
+        local args = {
+            [1] = "I'd now place my wrist over my watch, engulfing it in a mechanical glove",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(1.5)
+        local args = {
+            [1] = "I'd now raise my hand aiming towards them",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(1.5)
+        local args = {
+            [1] = "I'd press on a trigger using my thumb",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.5)
+        local args = {
+            [1] = "The glove disperses electrostatic and radiant energy from a 2-inch outdent on the glove",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.5)
+        local args = {
+            [1] = "A Coulomb field is generated, which is then affected by abrupt structural change",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.8)
+        local args = {
+            [1] = "The field transitions between two dynamically conjugate states of axisymmetric flow constantly",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.3)
+        local args = {
+            [1] = "It generates red hexagonal panels overlapping each other and acts as a shield",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "IC [Activate]",
+    function()
+        local args = {
+            [1] = "I'd now click on a flat embed on my wrist",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(1.5)
+        local args = {
+            [1] = "The hexagonal panels constrict and diminish",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(1.5)
+        local args = {
+            [1] = "The glove retracts and reforms into a watch again",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Havoc Mine",
+    function()
+        local args = {
+            [1] = "I'd reach into my suit, gripping something",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.9)
+        local args = {
+            [1] = "I'd trigger the device that I was clasping on",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(3)
+        local args = {
+            [1] = "I'd now hurl the cylindrical air-burst grenade into the air, dubbed Havoc Mine",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(3)
+        local args = {
+            [1] = "The grenade opens up, revealing several square cartridge slots",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(3.2)
+        local args = {
+            [1] = "The grenade begins to spin, spraying shrapnel all over them",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.8)
+        local args = {
+            [1] = "Multiple shrapnel shards now cause ruptures and fractures all over their body",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.8)
+        local args = {
+            [1] = "The grenade eventually falls after its shrapnel is diminished completely",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Pendragon Implosive",
+    function()
+        local args = {
+            [1] = "I'd reach into my suit, gripping something",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(1.5)
+        local args = {
+            [1] = "I'd trigger the device that I was clasping on",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.5)
+        local args = {
+            [1] = "I'd now hurl the imploding field grenade towards them, dubbed Pendragon Implosive",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.5)
+        local args = {
+            [1] = "The grenade creates a blinding spherical flash of light as a result of the Folkvangr Field",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(1.5)
+        local args = {
+            [1] = "Their lower half is absorbed by the sphere, as it numbs",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.8)
+        local args = {
+            [1] = "The sphere's expansion abruptly stops, and it then implodes vanishing along with the matter the sphere engulfed",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.3)
+        local args = {
+            [1] = "Air rushes to fill the empty space, thus causing a massive collapse effect",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.3)
+        local args = {
+            [1] = "The remaining half of their body distorts as a result of the shockwaves",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "VH Particle Cannon",
+    function()
+        local args = {
+            [1] = "I'd reach into my suit, gripping something",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.5)
+        local args = {
+            [1] = "I'd now take out a testbed model of a particle cannon, dubbed Vulcan-Hadron Particle Cannon",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+        local args = {
+            [1] = "I'd aim the VHPC towards their torso, intending to kill them",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(1.5)
+        local args = {
+            [1] = "I'd unhesitantly pull the trigger of the VHPC",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.8)
+        local args = {
+            [1] = "The electrons propagating through a magnetic field emit coherent photon beams via synchrotron radiation",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.5)
+        local args = {
+            [1] = "Individual particle beams within its rails collide and converge into a singular hadron beam",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.3)
+        local args = {
+            [1] = "Red and black chains of electricity surround the black, distorted beam",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.3)
+        local args = {
+            [1] = "The beam hurls towards them and accelerates to Mach 8",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2.5)
+        local args = {
+            [1] = "The beam blasts straight through their torso, creating a gaping hole in their abdomen",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
