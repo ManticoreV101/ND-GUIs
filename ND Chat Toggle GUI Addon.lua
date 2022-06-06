9a779faeefef1df8a10c272a7d105dd0b1d37468
Blue's Hue <76952738+ManticoreV101@users.noreply.github.com>
2022-06-06 08:33:19 +0800

    Update ND Chat Toggle GUI Addon.lua

--- ND Chat Toggle GUI Addon.lua
+++ ND Chat Toggle GUI Addon.lua
@@ -1,11 +1,11 @@
-local lib = loadstring(game:HttpGet"https://fluxteam.xyz/external-files/lib.lua")()
-local window = lib:CreateWindow('Chat Utility')
+local lib = loadstring(game:HttpGet "https://fluxteam.xyz/external-files/lib.lua")()
+local window = lib:CreateWindow("Chat Utility")
 
 local Players = game:GetService("Players")
 local lplay = Players.LocalPlayer
-local PlayerScripts = lplay ~= nil and lplay:FindFirstChild('PlayerScripts') or nil
-local ChatScript = PlayerScripts ~= nil and PlayerScripts:FindFirstChild('ChatScript') or nil
-local ChatMain = ChatScript ~= nil and ChatScript:FindFirstChild('ChatMain') or nil
+local PlayerScripts = lplay ~= nil and lplay:FindFirstChild("PlayerScripts") or nil
+local ChatScript = PlayerScripts ~= nil and PlayerScripts:FindFirstChild("ChatScript") or nil
+local ChatMain = ChatScript ~= nil and ChatScript:FindFirstChild("ChatMain") or nil
 
 local Quote = nil
 local Line = nil
@@ -13,15 +13,15 @@ local Square = nil
 
 function Mode(text)
     if Quote then
-        local text = tostring("` `"..text.."` `")
+        local text = tostring("` `" .. text .. "` `")
         return text
     end
     if Line then
-        local text = tostring('|◆| '..text..' |◆|')
+        local text = tostring("|◆| " .. text .. " |◆|")
         return text
     end
     if Square then
-        local text = tostring('['..text..']')
+        local text = tostring("[" .. text .. "]")
         return text
     end
 end
@@ -33,18 +33,23 @@ local function InitialiseReplace(ChatScript)
     local LastChangedText = tick()
     local PreviousText = ""
 
-    TextBox:GetPropertyChangedSignal("Text"):Connect(function()
-        LastChangedText = tick()
-    end)
-
-    local Checker = coroutine.create(function()
-        if TextBox.Text ~= "" or TextBox.Text ~= nil then
-            local ImprovedText = Mode(TextBox.Text)
-            print(ImprovedText,"Text")
-            TextBox.Text = ImprovedText
-            PreviousText = ImprovedText
+    TextBox:GetPropertyChangedSignal("Text"):Connect(
+        function()
+            LastChangedText = tick()
         end
-    end)
+    )
+
+    local Checker =
+        coroutine.create(
+        function()
+            if TextBox.Text ~= "" or TextBox.Text ~= nil then
+                local ImprovedText = Mode(TextBox.Text)
+                print(ImprovedText, "Text")
+                TextBox.Text = ImprovedText
+                PreviousText = ImprovedText
+            end
+        end
+    )
     coroutine.resume(Checker)
     return Checker
 end
@@ -58,14 +63,23 @@ local ChatBar = debug.getupvalue(ChatMains.FocusChatBar, 1)
 local TextBox = ChatBar:GetTextBox()
 TextBox.FocusLost:Connect(Caller)
 
-window:AddToggle("Speech", function(t)
-    Quote = t
-end)
+window:AddToggle(
+    "Speech",
+    function(t)
+        Quote = t
+    end
+)
 
-window:AddToggle("Action", function(t)
-    Line = t
-end)
+window:AddToggle(
+    "Action",
+    function(t)
+        Line = t
+    end
+)
 
-window:AddToggle("OOC", function(t)
-    Square = t
-end)
+window:AddToggle(
+    "OOC",
+    function(t)
+        Square = t
+    end
+)
