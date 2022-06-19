97e709bc735e16c928ceeb0344f5c2224d607d0f
AKIRA <76952738+ManticoreV101@users.noreply.github.com>
2022-06-19 19:46:24 +0800

    Add files via upload

---
+++ Combat Addon GUIs/ND Gore Combat GUI Addon.lua
@@ -0,0 +1,231 @@
+local lib = loadstring(game:HttpGet "https://raw.githubusercontent.com/ManticoreV101/ND-GUIs/main/UI%20Library%20Backup.lua")()
+local window = lib:CreateWindow("Gore Combat")
+
+window:AddButton(
+    "Crush Thyroid Gland",
+    function()
+        local args = {
+            [1] = "I'd now grip your neck firmly, tightening around it",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "I'd now position my thumbs on your thyroid gland",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "I'd press down, crushing your thyroid gland, and cutting your airflow"
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Sever Carotid Artery",
+    function()
+        local args = {
+            [1] = "I'd now raise my finger, intending to use it as a sharpener",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "I'd now angle my wrist towards your sternohyoid, preparing to strike",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "I'd extend my arm in a flash, piercing your throat",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "The pierce would cause intense bleeding, as I severed your external carotid artery",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Gauge Eyes Out",
+    function()
+        local args = {
+            [1] = "I'd plunge my fingers into your eyes, impaling you inside the occipital cavity",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "I'd rip your eyes out of your eye sockets",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Face Stomp",
+    function()
+        local args = {
+            [1] = "I'd sidekick your knee, tearing their anterior cruciate ligament, and causing you to fall",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "I'd now stomp on your face, thus causing it to cave in due to inertia",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "I'd now kick you in the jaw to maximize head movement thus causing your C2 to slide forward and sever your cervical vertebrae",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Crush Head",
+    function()
+        local args = {
+            [1] = "I'd now grip both sides of your temporal region",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "I'd now squeeze on it with full force, similar to that of a hydraulic press",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "The dual-layered graphene coated on my skin begins to act up, turning into diamene",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "Your eyeballs gauge out as your head is compressed",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "Eventually, your skull structure gives in, and your head is crushed by the pressure",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Rip Jaw",
+    function()
+        local args = {
+            [1] = "I'd now position my hands into your upper and lower jaw",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "I'd now simultaneously pull and push on the corresponding portions of their jaw",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "Their temporal mandibular joint begins to tear along with their masseter as the force exerted on their mandible was exorbitant",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "Their jaw gives in, wherein their skin, tissue, and muscles rip, leaving the lower portion of their jaw dangling",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "Sever Tongue",
+    function()
+        local args = {
+            [1] = "I'd now shove my hand into their mouth, leaving their jaw wide open",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "I'd now push further into the entrance of their esophagus, traversing my way to the end of their tongue",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "I'd now grip the furthest point, clenching on it tightly",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "I'd now tug on it with full force, ripping their tongue straight out",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
