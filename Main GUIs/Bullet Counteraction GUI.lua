10617c66c6038bf4706ddd5e11fe3dee133aa21d
AKIRA <76952738+ManticoreV101@users.noreply.github.com>
2022-06-24 20:37:37 +0800

    Create Bullet Counteraction GUI.lua

---
+++ Main GUIs/Bullet Counteraction GUI.lua
@@ -0,0 +1,231 @@
+local lib = loadstring(game:HttpGet "https://raw.githubusercontent.com/ManticoreV101/ND-GUIs/main/UI%20Library%20Backup.lua")()
+local window = lib:CreateWindow("Bullet Counteraction")
+
+window:AddButton(
+    "AP Round Head",
+    function()
+        local args = {
+            [1] = "The AP round now encounters the Nomex-Kevlar polymer, and as it nudges its way through, its velocity is diminished considerably",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The carbon-nanotube reinforced Dyneema fiber network in my mask absorbs the remaining kineticism, as the bullet lodged itself there",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "The accompanying compressed laminate sheets and artificially-coiled carbon-fiber muscle actuators reduce the kinetic shock to near zero"
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "AP Rounds Head",
+    function()
+        local args = {
+            [1] = "The AP rounds now encounter the Nomex-Kevlar polymer, and as they nudge their way through, their velocity is diminished considerably",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The carbon-nanotube reinforced Dyneema fiber network in my mask absorbs the remaining kineticism, as the bullets lodge themselves there",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "The accompanying compressed laminate sheets and artificially-coiled carbon-fiber muscle actuators reduce the kinetic shock to near zero",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "AP Round Torso",
+    function()
+        local args = {
+            [1] = "The AP round twists through my suit as it perforates through the silicon carbide discs",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The ceramic matrices and high-modulus polyethylene effectuates the bullet's loss in velocity",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The bullet is now lodged in the carbon nanotube-reinforced Dyneema coated in dual-layered graphene",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        
+        wait(2)
+
+        local args = {
+            [1] = "The microlattice absorbing pads and SHOCKtec padding absorb the majority of the impact shock",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "AP Rounds Torso",
+    function()
+        local args = {
+            [1] = "The AP rounds twist through my suit as they perforate through the silicon carbide discs",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The ceramic matrices and high-modulus polyethylene effectuates the bullets' loss in velocity",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The bullets are now lodged in the carbon nanotube-reinforced Dyneema coated in dual-layered graphene",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        
+        wait(2)
+
+        local args = {
+            [1] = "The microlattice absorbing pads and SHOCKtec padding absorb the majority of the impact shock",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "N-Round Head",
+    function()
+        local args = {
+            [1] = "The aluminum alloy matrix reinforced by nickel-coated, multi-walled carbon nanotubes in my mask immobilizes the bullet completely",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The polyethylene-wrapped silicon-carbide layer under the titanium alloy frame of my mask absorbs the kinetic shock",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "N-Rounds Head",
+    function()
+        local args = {
+            [1] = "The aluminum alloy matrix reinforced by nickel-coated, multi-walled carbon nanotubes in my mask immobilizes the bullets completely",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The polyethylene-wrapped silicon-carbide layer under the titanium alloy frame of my mask absorbs the kinetic shock",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "N-Round Torso",
+    function()
+        local args = {
+            [1] = "The tungsten carbide ceramic plating causes the bullet to lodge itself into the plating causing a dent",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The silver-cellulose aerogel along with optical fiber networks keep the structural integrity of the plating intact",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "The  blend of Nomex and Kevlar aramids, with polyethylene sheets sandwiching the plate padding reduces the impact shock",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
+window:AddButton(
+    "N-Rounds Torso",
+    function()
+        local args = {
+            [1] = "The tungsten carbide ceramic plating causes the bullets to lodge themselves into the plating causing a dent",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+        wait(2)
+
+        local args = {
+            [1] = "The silver-cellulose aerogel along with optical fiber networks keep the structural integrity of the plating intact",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+        wait(2)
+
+        local args = {
+            [1] = "The  blend of Nomex and Kevlar aramids, with polyethylene sheets sandwiching the plate padding reduces the impact shock",
+            [2] = "All"
+        }
+
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+    end
+)
